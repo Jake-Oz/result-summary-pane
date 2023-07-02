fe54a8f8a4c9e93fee6953dcf57a3d25e9e4e158
Jake-Oz <jakeandrach@me.com>
2023-07-02 15:57:30 +1000

    updated readme and screenshot

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -2,7 +2,7 @@
 
 const Button = () => {
   return (
-    <div className="flex justify-center cursor-pointer rounded-3xl bg-darkGreyBlue text-paleBlue text-center h-12 mt-2">
+    <div className="flex justify-center cursor-pointer rounded-3xl hover:bg-gradient-to-b from-lightSlateBlue to-lightRoyalBlue bg-darkGreyBlue text-paleBlue text-center h-12 mt-2">
       <button
         className="w-full"
         onClick={() => {
